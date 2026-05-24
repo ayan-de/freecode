d1c179384adba0679d73934d056a3d85a13a46fc
Ayan De <deayan252@gmail.com>
2026-05-24 20:01:11 +0530

    fix(web): adjust SandboxContainer rectangle properties for improved layout

--- apps/web/app/internal/components/diagrams/SandboxContainer.tsx
+++ apps/web/app/internal/components/diagrams/SandboxContainer.tsx
@@ -9,11 +9,11 @@ export function SandboxContainer() {
         y="20"
         width="800"
         height="605"
-        rx="16"
+        rx="0"
         className={styles.outerSandbox}
       />
-      <rect x="200" y="35" width="220" height="24" rx="6" fill="#121225" />
-      <text x="215" y="52" className={styles.outerSandboxTitle}>
+      <rect x="200" y="25" width="280" height="24" rx="6" fill="#121225" />
+      <text x="215" y="42" className={styles.outerSandboxTitle}>
         &gt;_ FREECODE CLI BACKEND (DAEMON)
       </text>
     </g>
